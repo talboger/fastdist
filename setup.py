d17032867bece0206a35cb759d8f9d72c8e5dcc6
talboger <tboger10@gmail.com>
2020-07-26 14:50:04 -0400

    create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fastdist",
+    version="1.0.0",
+    author="tal boger",
+    author_email="tboger10@gmail.com",
+    description="Faster distance calculations in python using numba",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/talboger/fastdist",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
