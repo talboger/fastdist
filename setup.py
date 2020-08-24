ed338cc07249d14a8e4f88783015dd0d1fb242e8
talboger <tboger10@gmail.com>
2020-08-24 13:24:29 -0400

    increment to v1.1.0 from v1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fastdist",
-    version="1.0.0",
+    version="1.1.0",
     author="tal boger",
     author_email="tboger10@gmail.com",
     description="Faster distance calculations in python using numba",
