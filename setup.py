e3420838943f78be5d53827dd305ca0051c14756
tal boger <61988768+talboger@users.noreply.github.com>
2021-04-19 11:56:37 -0400

    bump to version 1.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fastdist",
-    version="1.1.1",
+    version="1.1.2",
     author="tal boger",
     author_email="tboger10@gmail.com",
     description="Faster distance calculations in python using numba",
